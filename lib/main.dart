331e29c61a34744fdfa0957ee9370950bc7b151d
carlragas <carlragas@gmail.com>
2026-03-03 23:27:26 +0800

    Comment Change test and App Update

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:window_manager/window_manager.dart';
 import 'package:provider/provider.dart';
 import 'core/theme/theme_provider.dart';
 
+/* Testing comment */
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
